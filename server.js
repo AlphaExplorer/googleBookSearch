2987d5064977e27c6ebad05f8bc9e22950113125
Francisco Gonzalez <franciscogonzalez@Franciscos-MacBook-Pro.local>
2019-11-16 10:52:09 -0500

    Heroku

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
@@ -9,6 +8,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
+console.log("Starting in: ",process.env.NODE_ENV);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
